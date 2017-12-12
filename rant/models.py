623f332726b429fae250f4de6f1767c0eb034242
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-12-12 19:03:49 +0300

    Create Profile Model Create Profile model to define a user's profile information when using the application Create test case for the instance of Profile class Create test case for the get profiles class method that gets all the Profile objects from the database Create test case for the get single profile class method that gets a specific Profile object from the database Create migration to a database schema with Profile model

--- rant/models.py
+++ rant/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# Default image for a new profile
+DEFAULT = 'profile-pic/kakashi.jpg'
 
 # Create your models here.
+class Profile(models.Model):
+    '''
+    Class to define a user's profile
+    '''
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    profile_pic = models.ImageField(upload_to="profile-pic/", blank=True)
+
+    def __str__(self):
+        '''
+        Display for profiles in profile table
+        '''
+        return self.user.username
+
+    @classmethod
+    def get_profiles(cls):
+        '''
+        Fucntion that gets all the profiles in the app
+
+        Return
+            profiles : list of all Profile obejcts in the database
+        '''
+        profiles = Profile.objects.all()
+
+        return profiles
+
+    @classmethod
+    def get_single_profile(cls,user_id):
+        '''
+        Function that gets profiles of the specified user id
+
+        Args:
+            user_id : the user id
+
+        Returns
+            single_profile : Profile object with the specified user id
+        '''
+
+        single_profile = Profile.objects.get(user=user_id)
+
+        return single_profile
+
+
+# Create Profile when creating a User
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+# Save Profile when saving a User
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
--- rant/tests.py
+++ rant/tests.py
@@ -1,3 +1,49 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    '''
+    Test case for the Profile class
+    '''
+    def setUp(self):
+        '''
+        Method that creates an instance of Profile class
+        '''
+        # Create instance of User class
+        self.james = User(username='Kiki')
+        self.james.save()
+
+        # Create instance of Profile class
+        self.new_profile = Profile(user=self.james)
+
+    def test_instance(self):
+        '''
+        Test case to check if self.new_profile in an instance of Profile class
+        '''
+        self.assertTrue( isinstance(self.new_profile, Profile) )
+
+    def test_get_profiles(self):
+        '''
+        Test case to check if all profiles are gotten from the database
+        '''
+        gotten_profiles = Profile.get_profiles()
+
+        profiles = Profile.objects.all()
+
+        self.assertTrue( len(gotten_profiles) == len(profiles))
+
+    def test_get_single_profile(self):
+        '''
+        Test case to check if a single profile is gotten from the database
+        '''
+        self.jane = User(username="ja-ne")
+        self.jane.save()
+
+        self.test_profile = Profile(user=self.jane)
+
+        gotten_profile = Profile.get_single_profile(self.jane.id)
+
+        self.assertTrue( isinstance(gotten_profile, Profile))
+
