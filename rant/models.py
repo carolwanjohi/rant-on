8d879f86e065b236dd1b8b8a0b5a46f24a6f7646
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-12-13 11:59:38 +0300

    Create Rant Model * Create Rant model to define a rant written by the user * Create test case for the instance of the Rant class * Create test case for the get rants class method that gets all the rants from the database * Create test case for the get single rant class method that gets a rant with a specified id * Create test case for the get user rants class method that gets rants belonging to a specific user * Create migration to a database schema with the Rant model

--- rant/models.py
+++ rant/models.py
@@ -17,7 +17,7 @@ class Profile(models.Model):
 
     def __str__(self):
         '''
-        Display for profiles in profile table
+        Display for Profile Object in Profile table
         '''
         return self.user.username
 
@@ -36,7 +36,7 @@ class Profile(models.Model):
     @classmethod
     def get_single_profile(cls,user_id):
         '''
-        Function that gets profiles of the specified user id
+        Function that gets a profile with the specified user id
 
         Args:
             user_id : the user id
@@ -49,7 +49,6 @@ class Profile(models.Model):
 
         return single_profile
 
-
 # Create Profile when creating a User
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -60,4 +59,65 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+class Rant(models.Model):
+    '''
+    Class to define a rant
+    '''
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    rant_title = models.CharField(max_length=255)
+
+    rant_content = models.TextField()
+
+    def __str__(self):
+        '''
+        Display for Rant Object in Rant table
+        '''
+        return self.author.username + '\'s rant'
+
+
+    @classmethod
+    def get_rants(cls):
+        '''
+        Function that gets all the Rant objects in the database
+
+        Return
+            rants : list of all Rant objects in the database
+        '''
+        rants = Rant.objects.all()
+
+        return rants
+
+    @classmethod
+    def get_single_rant(cls,rant_id):
+        '''
+        Function that gets a rant with the specified rant id
+
+        Args:
+            rant_id : the rant's id
+
+        Returns
+            single_rant : Rant object with the specified rant id
+        '''
+        single_rant = Rant.objects.get(id=rant_id)
+
+        return single_rant
+
+    @classmethod
+    def get_user_rants(cls, user_id):
+        '''
+        Function that gets rant objects with the specified user id
+
+        Args:
+            user_id : the user id
+
+        Returns
+            user_rants : List of Rant objects with the specified user id
+        '''
+        user_rants = Rant.objects.filter(author=user_id)
+
+        return user_rants
+
+
 
--- rant/tests.py
+++ rant/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, Rant
 
 # Create your tests here.
 class ProfileTestClass(TestCase):
@@ -46,4 +46,73 @@ class ProfileTestClass(TestCase):
         gotten_profile = Profile.get_single_profile(self.jane.id)
 
         self.assertTrue( isinstance(gotten_profile, Profile))
+
+class RantTestClass(TestCase):
+    '''
+    Test case for the Rant class
+    '''
+    def setUp(self):
+        '''
+        Method that creates an instance of Rant class
+        '''
+        # Create instance of User class
+        self.james = User(username='Kiki')
+        self.james.save()
+
+        self.new_rant = Rant(author=self.james.profile ,rant_title='Cupcake', rant_content='Cupcake jelly beans chocolate cake lollipop candy powder tootsie roll dragée croissant.')
+
+    def test_instance(self):
+        '''
+        Test case to check if self.new_rant in an instance of Rant class
+        '''
+        self.assertTrue( isinstance(self.new_rant, Rant) )
+
+    def test_get_rants(self):
+        '''
+        Test case to check if all rants are gotten from the database
+        '''
+        self.new_rant.save()
+
+        gotten_rants = Rant.get_rants()
+
+        rants = Rant.objects.all()
+
+        self.assertTrue( len(gotten_rants) == len(rants))
+
+    def test_get_single_rant(self):
+        '''
+        Test case to check if a single rant is gotten from the database
+        '''
+        self.new_rant.save()
+
+        gotten_rant = Rant.get_single_rant(self.new_rant.id)
+
+        self.assertTrue( isinstance(gotten_rant, Rant))
+
+
+    def test_get_user_rants(self):
+        '''
+        Test case to check if rants for a specific user are gotten from the database
+        '''
+        self.new_rant.save()
+
+        self.jane = User(username="ja-ne")
+        self.jane.save()
+
+        self.test_rant = Rant(author=self.jane.profile, rant_title='Muffin', rant_content='I love muffin cookie lemon drops dragée. Caramels macaroon I love. Dragée jelly beans topping.')
+        self.test_rant.save()
+
+        gotten_user_rants = Rant.get_user_rants(self.jane.id)
+
+        rants = Rant.objects.all()
+
+        self.assertTrue( len(gotten_user_rants) != len(rants))
+
+
+
+
+
+
+
+
 
