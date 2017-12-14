3e71850b86c9235584e22c690791fa1270015752
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-12-14 20:54:17 +0300

    Minor change * Add Reaction model to admin.py

--- rant/admin.py
+++ rant/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Profile, Rant
+from .models import Profile, Rant, Reaction
 
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Rant)
+admin.site.register(Reaction)
+
 
